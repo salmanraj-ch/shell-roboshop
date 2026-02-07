261ac22c5c178480efb61fdf68f5760f128eaa69
Salman Raj Chandole <salmanrajchandole@gmail.com>
2026-02-07 23:51:54 +0530

    catalogue.sh

--- catalogue.sh
+++ catalogue.sh
@@ -50,17 +50,17 @@ VALIDATE $? "Downloading catalogue code"
 cd /app 
 VALIDATE $? "Moving to app directory"
 
-unzip /tmp/catalogue.zip
+unzip /tmp/catalogue.zip &>>$LOGS_FILE
 VALIDATE $? "Unzip catalogue code"
 
-npm install 
+npm install &>>$LOGS_FILE
 VALIDATE $? "installing dependencies"
 
 cp catalogue.service /etc/systemd/system/catalogue.service
 VALIDATE $? "Created systemctl service"
 
 systemctl daemon-reload
-systemctl enable catalogue 
+systemctl enable catalogue &>>$LOGS_FILE
 systemctl start catalogue
 VALIDATE $? "Starting and enabling catalogue"
 
