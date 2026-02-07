ddad36820edacbca83c8b16d05af84bfa6f424bd
Salman Raj Chandole <salmanrajchandole@gmail.com>
2026-02-07 23:56:12 +0530

    catalogue.sh

--- catalogue.sh
+++ catalogue.sh
@@ -50,6 +50,9 @@ VALIDATE $? "Downloading catalogue code"
 cd /app 
 VALIDATE $? "Moving to app directory"
 
+rm -rf /app/*
+VALIDATE $? "Removing existing code"
+
 unzip /tmp/catalogue.zip &>>$LOGS_FILE
 VALIDATE $? "Unzip catalogue code"
 
